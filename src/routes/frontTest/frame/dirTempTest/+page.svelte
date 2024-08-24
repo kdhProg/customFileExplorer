b87fa309ded92efe8d1c87886c28f5cadd9b9946
notePad <jei36bob@naver.com>
2024-08-24 15:45:08 +0900

    test

--- src/routes/frontTest/frame/dirTempTest/+page.svelte
+++ src/routes/frontTest/frame/dirTempTest/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
     import Folder from '$lib/components/Folder.svelte';
     import { drives } from '$lib/store';
   </script>
@@ -9,19 +8,3 @@
     {/each}
   </div>
   
-=======
-  import Folder from "$lib/components/Folder.svelte";
-  import { drives } from "$lib/store";
-</script>
-
-<button
-  on:click={() => {
-    console.log($drives);
-  }}>Show store</button
->
-<div>
-  {#each Object.keys($drives) as drive}
-    <Folder path={drive} name={drive} items={$drives[drive]} />
-  {/each}
-</div>
->>>>>>> 21c21625ac5d068dd4a88d9246165af68b17615b
